ef34713aa9f7b7051e14b3dceeae5fbb219504ab
Byosphere <dreamlike.swarm@gmail.com>
2015-07-01 18:55:34 +0200

    Intitial commit

---
+++ database/migrations/2015_07_01_160345_create_reservations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('reservations', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->date('debut');
+			$table->string('velo');
+			$table->boolean('valide');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('reservations');
+	}
+
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('app')
+
+@section('content')
+<div class="site-wrapper">
+	<div class="cover">
+		<img src="{{asset('/img/home.jpg')}}" alt="">
+		
+	</div>
+	<div class="tab">
+		
+	</div>
+</div>
+@endsection
