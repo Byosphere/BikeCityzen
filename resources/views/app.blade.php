0c3fd14756f30ab0f5bdfab52ef654cf2bd4bea1
Byosphere <dreamlike.swarm@gmail.com>
2015-07-02 18:44:47 +0200

    design front

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,36 +20,15 @@
 	<![endif]-->
 </head>
 <body>
-	<nav class="navbar navbar-default navbar-fixed-top">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-			</div>
-
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}">Accueil</a></li>
-				</ul>
-
-				<ul class="nav navbar-nav navbar-right">
-					@if (Auth::guest())
-						<li><a href="{{ url('/auth/login') }}">Se connecter</a></li>
-						<li><a href="{{ url('/auth/register') }}">S'inscrire</a></li>
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
-			</div>
+	<nav class="navigation">
+		<div class="container">
+			<div class="head-logo"><b>BIKE</b>CITYZEN</div>
+			<ul class='nav nav-tabs'>
+				<li><a href="">Accueil</a></li>
+				<li><a href="">Location</a></li>
+				<li><a href="">Réparation</a></li>
+				<li><a href="">contact</a></li>
+			</ul>
 		</div>
 	</nav>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,25 @@
 @section('content')
 <div class="site-wrapper">
 	<div class="cover">
-		<img src="{{asset('/img/home.jpg')}}" alt="">
-		
+		<div class="container">
+			<div class="headlines">
+		        <h1 class="title">APPRÉCIEZ VOS DÉPLACEMENTS A DEUX ROUES</h1>
+		        <p class="subtitle">LOCATION ET RÉPARATION DE VÉLOS SUR LYON</p>
+	
+			</div>
+		</div>
 	</div>
-	<div class="tab">
-		
+	<div class="tab location">
+		<h2>PAGE DETAILS LOCATION</h2>
 	</div>
+	<div class="tab reparation">
+		<h2>PAGE DETAILS REPARATION</h2>
+	</div>
+	<div class="tab partenaire">
+		<h2>PAGE DETAILS PARTENAIRES</h2>
+	</div>
+	<div class="tab contact">
+		<h2>PAGE CONTACT</h2>
+	</div>	
 </div>
 @endsection
